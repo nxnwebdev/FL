c7ed074f253379f990cdd868041bb1defd5383b3
nxn <kovalenko.wpr@gmail.com>
2022-05-30 19:15:02 +0300

    add mobile menu and all

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ $(document).ready(function(){
      $('.ma-header_col-menu').toggleClass('d-block');
      $('.ma-header__menu').toggleClass('d-block');
      $('.ma-header__menu_item-sub').toggleClass('ma-header__menu_item-sub-mobile');
+     $('.ma-header_col-phones').toggleClass('order-7 col-6 py-3');
+     $('.ma-header_col-address').toggleClass('order-9 col-6 d-block py-3');
+     $('.ma-header_col-time').toggleClass('order-8 col-6 d-block py-3');
+     $('.ma-header_col-order').toggleClass('order-12 d-block');
+     $('.ma-header_col-socials').toggleClass('order-10 col-6  d-block');
+     $('.ma-header_col-search').toggleClass('order-11 d-block m-auto');
     });
 
     $('.ma-header__menu_item-sub').on('click',function(e){
